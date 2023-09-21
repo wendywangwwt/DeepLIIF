c8feae4c9275378b8b32c3adf963aa49ccc042fc
Wendy Wang <wanting.wang@columbia.edu>
2023-09-21 00:50:14 -0400

    added cpu test

--- tests/test_cli_train.py
+++ tests/test_cli_train.py
@@ -4,6 +4,21 @@ import torch
 import pytest
 
 available_gpus = torch.cuda.device_count()
+
+def test_cli_train(tmp_path, model_info):
+    dirs_input = model_info['dir_input_train']
+    for dir_input in dir_inputs:
+        dir_save = tmp_path
+        
+        fns_input = [f for f in os.listdir(dir_input + '/train') if os.path.isfile(os.path.join(dir_input + '/train', f)) and f.endswith('png')]
+        num_input = len(fns_input)
+        assert num_input > 0
+        
+        res = subprocess.run(f'python cli.py train --model {model_info["model"]} --dataroot {dir_input} --name test_local --batch-size 1 --num-threads 0 --checkpoints-dir {dir_save} --remote True --n-epochs 1 --n-epochs-decay 1 --seg-gen {"noseg" not in dir_input}',shell=True)
+        assert res.returncode == 0
+
+        res = subprocess.run(f'python cli.py serialize --models-dir {dir_save} --output-dir {dir_save}',shell=True)
+        assert res.returncode == 0
     
 
 def test_cli_train_single_gpu(tmp_path, model_info, model_type):
