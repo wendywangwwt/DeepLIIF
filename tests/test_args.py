96b68abfd5133c9aa24b21e0fb74a29046f428cb
Wendy Wang <wanting.wang@columbia.edu>
2024-11-27 04:31:41 +0000

    added hardware info

--- tests/test_args.py
+++ tests/test_args.py
@@ -1,5 +1,14 @@
-
+import torch
+from cpuinfo import get_cpu_info
 
 def test_print_argument(model_type,model_dir):
-    print ("Displaying model_type: %s" % model_type)
-    print ("Displaying model_dir: %s" % model_dir)
+    print("Displaying model_type: %s" % model_type)
+    print("Displaying model_dir: %s" % model_dir)
+    print("********** Hardware Info *********")
+    print('GPU:')
+    print(torch.cuda.get_device_name(),torch.cuda.device_count())
+    cpu_info = get_cpu_info()
+    del cpu_info['flags']
+    print('CPU:')
+    print(cpu_info)
+    print('********** Hardware Info End *********')
